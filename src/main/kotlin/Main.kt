21d1397389a7f214612112a1d9657879ec1b0b28
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-11-03 15:02:07 +0200

    LateInItVariables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
 fun main(args: Array<String>) {
 
-    println("Hello World!")
+//    println("Hello World!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    Proprieties()
+//
+//      val circle = Circle.randomCircle()
+//      val rectangle = Rectangle.randomRectangle()
 
-    Proprieties()
-
-      val circle = Circle.randomCircle()
-      val rectangle = Rectangle.randomRectangle()
+    Lateinit().initializeName()
 
 }
