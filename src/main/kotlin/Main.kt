7656c924105f0e0070ff76890ac434e7a6128256
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-11-09 23:48:25 +0200

    Singleton in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,23 @@ fun main(args: Array<String>) {
 //      val rectangle = Rectangle.randomRectangle()
 
       Warehouse()
+
+
+      // Singleton
+      Singleton.printVarName()
+      Singleton.variableName = "New Name"
+
+      var a = A()
+}
+
+class A {
+
+      init {
+            println("Class init method. Singleton variableName property : ${Singleton.variableName}")
+            Singleton.printVarName()
+      }
+
+      /// Singleton
 }
 
 
